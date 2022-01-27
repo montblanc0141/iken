30b33c45161196dc6b28b44637aa77e1ce2bb2fc
montblanc0141 <mh5.4wasugi10comaru@gmail.com>
2022-01-27 23:16:03 +0900

    Add Question and Answer TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // FirebaseFirestore firestore = FirebaseFirestore.instance;
   runApp(const MyApp());
 }
 
@@ -13,15 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -48,68 +45,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // String _question = "question";
 
-  void _incrementCounter() {
+  // void _addOpinion() {
+  //   setState(() {
+  //     _question;
+  //   });
+  // }
+  final messageTextController1 = TextEditingController();
+  final messageTextController2 = TextEditingController();
+
+  String _question = '';
+  String _answer = '';
+
+  void _handleText(String e) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _question = e;
     });
+  }
+
+  void _handleText2(String e) {
+    setState(() {
+      _answer = e;
+    });
+  }
+
+  void _addOpinion() async {
+                // ドキュメント作成
+                await FirebaseFirestore.instance
+                    .collection('opinions') // コレクションID
+                    .doc() // ここは空欄だと自動でIDが付く
+                    .set({
+                  'question': _question,
+                  'answer': _answer,
+                  'userId': 1
+                });
+                messageTextController1.clear(); //questionを空欄に
+                messageTextController2.clear(); //answerを空欄に
+                
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Text(
+            //   _text,
+            //   style: const TextStyle(
+            //       color: Colors.blueAccent,
+            //       fontSize: 30.0,
+            //       fontWeight: FontWeight.w500
+            //   ),
+            // ),
+            TextField(
+              enabled: true,
+              // 入力数
+              maxLength: 50,
+              style: const TextStyle(color: Colors.black),
+              obscureText: false,
+              maxLines: null ,
+              decoration: const InputDecoration(
+              icon: Icon(Icons.question_answer),
+              hintText: 'Put a question',
+              labelText: 'Question',
+              ),
+              controller: messageTextController1,
+              onChanged: _handleText,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              enabled: true,
+              // 入力数
+              maxLength: 50,
+              style: const TextStyle(color: Colors.black),
+              obscureText: false,
+              maxLines: null ,
+              decoration: const InputDecoration(
+              icon: Icon(Icons.question_answer),
+              hintText: 'Put an answer',
+              labelText: 'Answer',
+              ),
+              controller: messageTextController2,
+              onChanged: _handleText2,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            ElevatedButton
+          (
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: const Text('Add Opinion'),
+              onPressed: _addOpinion,
+            )
+          ])
+      ) ,
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), 
     );
   }
 }
